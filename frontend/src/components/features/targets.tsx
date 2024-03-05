e4f5ecfeadca791b254719b796968c11f87f0cfd
Gab Labelle <gab@visualise.ca>
2024-03-05 09:26:47 -0500

    Now feeding data to the table.

--- frontend/src/components/features/targets.tsx
+++ frontend/src/components/features/targets.tsx
@@ -1,52 +1,102 @@
-import {
-  Table,
-  TableCaption,
-  TableContainer,
-  Tbody,
-  Td,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { useMemo } from "react";
+import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "../ui/table";
+import monthlyTargets from "../../../data/monthlyTargets.json";
 
-// Starting from the simple Table variant example
-// https://chakra-ui.com/docs/components/table#simple-table
+interface MonthlyTarget {
+  month: number;
+  year: number;
+  beginningMRR: number;
+  newBusinessMRR: number;
+  churnRate: number;
+  grossChurnedMRR: number;
+  expansionRate: number;
+  expansionMRR: number;
+  endingMRR: number;
+}
+
+// TODO Avoid as many loops as possible,
+// and keep it readable.
+function normalizeMonthlyTargets(data: MonthlyTarget[]) {
+  // Using a Mao to guarantee the iteration order when mapping in JSX.
+  const normalizedData = new Map<
+    keyof Omit<MonthlyTarget, "year">,
+    { id: string; value: number | string }[]
+  >();
+
+  data.forEach((monthlyTarget, index) => {
+    // Create an array of keys from the object
+    const keys = Object.keys(monthlyTarget) as Array<keyof MonthlyTarget>;
+
+    // We need to transform the shape of
+    // our data to be suitable for our table
+    keys.forEach((key) => {
+      const isFirstItem = index === 0;
+      const isYear = key === "year";
+      const isMonth = key === "month";
+
+      // Create the keys in the Map
+      if (isFirstItem && !isYear) {
+        normalizedData.set(key, []);
+      }
+
+      // Feed the map with the data, as per the above typing,
+      // we omit the year row in the normalizedData Map
+      if (!isYear) {
+        const value = monthlyTarget[key];
+        // Need to create IDs I can use when mapping
+        // the data in JSX.
+        // Using underscore here because some values
+        // are negative numbers.
+
+        // For the month row, we'll use the IDs as values
+        const cellData = {
+          id: `${key}-${monthlyTarget["year"]}-${monthlyTarget["month"]}`,
+          value: isMonth ? `${monthlyTarget["year"]}-${value}` : value,
+        };
+        normalizedData.get(key)?.push(cellData);
+      }
+    });
+  });
+
+  return normalizedData;
+}
 
 export function Targets() {
+  // Memoize results from normalization function
+  const normalizedMonthlyTargets = useMemo(
+    () => normalizeMonthlyTargets(monthlyTargets),
+    [monthlyTargets]
+  );
+
   return (
     <TableContainer>
       <Table variant="simple">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
         <Thead>
           <Tr>
             <Th colSpan={3}>Targets</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
-          </Tr>
-          <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td isNumeric>0.91444</Td>
-          </Tr>
+          {Array.from(normalizedMonthlyTargets).map((row, index) => {
+            const isFirstItem = index === 0;
+            const [rowId, cells] = row;
+            console.log({ rowId, cells });
+            // We know all ids are unique because our array is
+            // derived from a Map.
+            return (
+              <Tr key={rowId}>
+                {/* TODO transform rowId into a proper "copy" string */}
+                <Td key={rowId}>{isFirstItem ? "" : rowId}</Td>
+                {cells.map((cell) => {
+                  const { id, value } = cell;
+                  console.log("cell", { id, value });
+                  const key = isFirstItem ? value : id;
+                  return <Td key={key}>{value}</Td>;
+                })}
+              </Tr>
+            );
+          })}
         </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
-          </Tr>
-        </Tfoot>
       </Table>
     </TableContainer>
   );
---
+++ frontend/src/components/ui/table.tsx
@@ -0,0 +1,11 @@
+export {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
